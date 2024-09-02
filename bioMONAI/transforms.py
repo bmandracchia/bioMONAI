36036cbc0b4422d8c94d76572217e2772949f917
bmandracchia <biagio.mandracchia@gmail.com>
2024-09-02 20:11:20 +0200

    update core and visualize

--- bioMONAI/transforms.py
+++ bioMONAI/transforms.py
@@ -7,8 +7,9 @@ __all__ = ['RandCrop2D', 'RandCropND', 'RandCropND_T', 'RandFlip', 'RandRot90']
 from fastai.vision.all import *
 from fastai.data.all import *
 from monai.transforms import SpatialCrop, Flip, Rotate90
+from numpy import percentile, isscalar, float32 as np_float32
 
-# %% ../nbs/05_transforms.ipynb 7
+# %% ../nbs/05_transforms.ipynb 11
 def _process_sz(size, ndim=3):
     if isinstance(size,int): 
         size=(size,)*ndim
@@ -19,7 +20,7 @@ def _get_sz(x):
     if not isinstance(x, Tensor): return fastuple(x.size)
     return fastuple(getattr(x, 'img_size', getattr(x, 'sz', (x.shape[1:])))) # maybe it should swap x and y axes 
 
-# %% ../nbs/05_transforms.ipynb 11
+# %% ../nbs/05_transforms.ipynb 14
 class RandCrop2D(RandTransform):
     "Randomly crop an image to `size`"
     split_idx,order = None,1
@@ -49,7 +50,7 @@ class RandCrop2D(RandTransform):
     def encodes(self, x):
         return SpatialCrop(roi_center=self.ctr, roi_size=self.size, lazy=self.lazy)(x)
 
-# %% ../nbs/05_transforms.ipynb 13
+# %% ../nbs/05_transforms.ipynb 15
 class RandCropND(RandTransform):
     """
     Randomly crops an ND image to a specified size.
@@ -102,7 +103,7 @@ class RandCropND(RandTransform):
         return SpatialCrop(roi_start=self.tl, roi_end=self.br, lazy=self.lazy)(x)
     
 
-# %% ../nbs/05_transforms.ipynb 16
+# %% ../nbs/05_transforms.ipynb 17
 class RandCropND_T(RandTransform):
     """
     Randomly crops an ND image to a specified size.
@@ -198,7 +199,7 @@ class RandFlip(RandTransform):
         else:
             return x
 
-# %% ../nbs/05_transforms.ipynb 22
+# %% ../nbs/05_transforms.ipynb 21
 class RandRot90(RandTransform):
     """
     Randomly rotate an ND image by 90 degrees in the plane specified by axes.
